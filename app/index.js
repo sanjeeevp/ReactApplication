0163968756811a04269ac86ed651a9be24be486a
SANJEEV PAUDEL <sanpjava@gmail.com>
2018-04-11 18:51:06 -0500

    updated the files

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
-import App from '../app/components/App'
-import React from 'react'
-import {render} from 'react-dom'
-
-render(<App />, document.getElementById('app'))
+import React from "react";
+import ReactDOM from "react-dom";
+import App from '../app/components/App';
+import './index.css';
+ 
+ReactDOM.render(
+  <App />, 
+  document.getElementById("root")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,11 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
+            },
+            {
+              test: /\.css/,
+              exclude: /node_modules/,
+              loaders: ['style-loader', 'css-loader']
             }
         ]
     },
